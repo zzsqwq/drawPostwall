173ad56b57e1171b96d772c3533666a86e0f6913
zs <2459958352@qq.com>
2022-01-17 20:20:33 +0800

    add scale 1.5

--- main.py
+++ main.py
@@ -16,19 +16,26 @@ import requests
 from PIL import Image, ImageDraw, ImageFont
 from flask import Flask, request
 
+dy = 1920/1280
+dx = 1080/720
+
 avatar_size = (180, 180)
 font_size = 20
 img_size = (1080, 720)
+scale = 1.5
 
 
 def cv2ImgAddText(img, text, left, top, textColor=(0, 255, 0), textSize=20):
     if isinstance(img, np.ndarray):  # 判断是否OpenCV图片类型
         img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
     # 创建一个可以在给定图像上绘图的对象
     draw = ImageDraw.Draw(img)
+
     # 字体的格式
     fontStyle = ImageFont.truetype(
         "./font/SourceHanSansCN/SourceHanSansCN-Regular.ttf", textSize, encoding="utf-8")
+
     # fontStyle = ImageFont.truetype(
     #    "./font/EmojiOneColor-SVGinOT.ttf", textSize)
     # 绘制文本
@@ -48,11 +55,12 @@ def getAvatar(qq_num):
 
 def getCircleAvatar(qq_num, size=(128, 128)):
     avatar_img = getAvatar(qq_num)
-    avatar_img = cv2.resize(avatar_img, dsize=size)
+    avatar_img = cv2.resize(avatar_img, dsize=(int(size[0]*scale), int(size[1]*scale)))
     rows, cols, channel = avatar_img.shape
     img_circle = np.zeros((rows, cols, 1), np.uint8)
     img_circle[:, :, :] = 255  # 设置为全透明
-    img_circle = cv2.circle(img_circle, (cols // 2, rows // 2), min(rows, cols) // 2, (0), -1)
+    img_circle = cv2.circle(img_circle, (cols // 2, rows // 2), min(rows, cols) // 2, 0, -1)
+
     for i in range(rows):
         for j in range(cols):
             if img_circle[i][j] == 255:
@@ -63,7 +71,7 @@ def getCircleAvatar(qq_num, size=(128, 128)):
     return avatar_img
 
 
-def getQrcodeImg(size=(192, 192)):
+def getQrcodeImg(size=(288, 288)):
     qr_img = cv2.imread("./qrcode.png")
     qr_img = cv2.resize(qr_img, dsize=size)
     return qr_img
@@ -85,23 +93,24 @@ def drawDashLine(img, pointx, pointy, decay=10):
 def get_img(post_data):
     keys = post_data.keys()
 
-    img = np.zeros((img_size[0], img_size[1], 3), np.uint8)
+    img = np.zeros((int(img_size[0]*scale), int(img_size[1]*scale), 3), np.uint8)
     img.fill(255)
 
     # add avatar
+
     print(type(post_data))
     print(type(post_data["post_contact_qq"]))
     if ("post_contact_qq" in keys) and len(post_data["post_contact_qq"]) != 0:
         avatar_img = getCircleAvatar(post_data["post_contact_qq"], size=avatar_size)
     else:
         avatar_img = getCircleAvatar("2825467691", size=avatar_size)
-    img[60:60 + avatar_size[0], 270:270 + avatar_size[1], :] = avatar_img
+    img[int(60*scale):int(60*scale) + int(avatar_size[0]*scale), int(270*scale):int(270*scale) + int(avatar_size[1]*scale), :] = avatar_img
 
     # add time
     real_time = post_data["post_date"]
     post_time = "投稿时间：" + real_time[0:4] + "/" + real_time[5:7] + "/" + real_time[8:10]
     print(post_time)
-    img = cv2ImgAddText(img, post_time, 250, 250, (128, 128, 128), textSize=20)
+    img = cv2ImgAddText(img, post_time, int(250*scale), int(250*scale), (128, 128, 128), textSize=int(20*scale))
 
     post_type = "[" + post_data["post_type"] + "]" + post_data["post_title"]
 
@@ -111,13 +120,13 @@ def get_img(post_data):
 
     type_len = len(res) * 25 + (len(post_type) - len(res)) * 50
     print(type_len)
-    img = cv2ImgAddText(img, post_type, (img_size[1] - type_len) // 2, 280, (0, 0, 255), textSize=50)
+    img = cv2ImgAddText(img, post_type, int((img_size[1] - type_len) * scale // 2), int(280*scale), (0, 0, 255), textSize=int(50 * scale))
 
     # post_title = post_data["post_title"]
     # img = cv2ImgAddText(img, post_title, 40 + font_size * len(post_type) + 70, 50, (0, 0, 255))
     # img = drawDashLine(img, (24, 180), (456, 180))
     # add split line
-    cv2.line(img, (40, 370), (680, 370), (0, 0, 0), thickness=2)
+    cv2.line(img, (int(40*scale), int(370*scale)), (int(680*scale), int(370*scale)), (0, 0, 0), thickness=int(2*scale))
 
     # add detail text
     post_text = post_data["post_text"]
@@ -137,27 +146,27 @@ def get_img(post_data):
             len_a = 0
             post_text_space += "\n"
 
-    img = cv2ImgAddText(img, post_text_space, 42, 390, (0, 0, 0), textSize=42)
+    img = cv2ImgAddText(img, post_text_space, int(42*scale), int(390*scale), (0, 0, 0), textSize=int(42*scale))
 
-    cv2.line(img, (40, 830), (680, 830), (0, 0, 0), thickness=2)
+    cv2.line(img, (int(40*scale), int(830*scale)), (int(680*scale), int(830*scale)), (0, 0, 0), thickness=int(2*scale))
     if ("post_contact_qq" in keys) and len(post_data["post_contact_qq"]) != 0:
         contact_qq = "Q Q：" + post_data["post_contact_qq"]
     else:
         contact_qq = "Q Q：" + "未填写"
-    img = cv2ImgAddText(img, contact_qq, 100, 880, (0, 0, 255), 22)
+    img = cv2ImgAddText(img, contact_qq, int(100*scale), int(880*scale), (0, 0, 255), int(22*scale))
     if ("post_contact_wechat" in keys) and len(post_data["post_contact_wechat"]) != 0:
         contact_wechat = "微信：" + post_data["post_contact_wechat"]
     else:
         contact_wechat = "微信：" + "未填写"
-    img = cv2ImgAddText(img, contact_wechat, 100, 930, (0, 0, 255), 22)
+    img = cv2ImgAddText(img, contact_wechat, int(100*scale), int(930*scale), (0, 0, 255), int(22*scale))
     if ("post_contact_tel" in keys) and len(post_data["post_contact_tel"]) != 0:
         contact_tel = "联系电话：" + post_data["post_contact_tel"]
     else:
         contact_tel = "联系电话：" + "未填写"
-    img = cv2ImgAddText(img, contact_tel, 100, 980, (0, 0, 255), 22)
+    img = cv2ImgAddText(img, contact_tel, int(100*scale), int(980*scale), (0, 0, 255), int(22*scale))
 
     qr_img = getQrcodeImg()
-    img[850:1042, 440:632, :] = qr_img
+    img[int(850*scale):int(1042*scale), int(440*scale):int(632*scale), :] = qr_img
 
     return img
 
@@ -180,7 +189,6 @@ def draw_image():
     image_code = str(base64.b64encode(image))[2:-1]
     # print(image_code)
     return image_code
-
 
 if __name__ == '__main__':
 
